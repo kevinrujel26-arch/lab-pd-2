b9096c3de8ff137fd69a54a1fd6fa69158ad4a28
Kevin Rujel <Kevinrujel26@gmail.com>
2026-04-25 18:41:46 -0500

    se actualizo la clase pato

--- src/pato/Pato.java
+++ src/pato/Pato.java
@@ -1,8 +1,30 @@
-
 package pato;
 
 public abstract class Pato {
+
     Volable volable;
     Cuaqueable cuaqueable;
- 
+
+    public void setVolable(Volable fb) {
+        this.volable = fb;
+    }
+
+    public void setCuaqueable(Cuaqueable qb) {
+        this.cuaqueable = qb;
+    }
+
+    public abstract void mostrar();
+
+    public void realizarVuelo() {
+        volable.volar();
+    }
+
+    public void realizarCuaqueo() {
+        cuaqueable.cuaquear();
+    }
+
+    public void nadar() {
+        System.out.println("TODOS LOS PATOS NADAN");
+    }
+
 }
