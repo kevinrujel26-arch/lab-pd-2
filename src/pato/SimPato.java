a8a34cf129d79ec777d64b967ac3899b326d009a
Kevin Rujel <Kevinrujel26@gmail.com>
2026-04-25 22:34:11 -0500

    se llama a los patos uno por uno y cada uno de los patos con sus respectivos metodos de mostrar,realizarcuaqueo, realizar vuelo y nadar, asi para cada pato creado

--- src/pato/SimPato.java
+++ src/pato/SimPato.java
@@ -7,7 +7,34 @@ public class SimPato {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pato patoReal = new PatoReal();
+        patoReal.mostrar();
+        patoReal.realizarCuaqueo();
+        patoReal.realizarVuelo();
+        patoReal.nadar();
+
+        System.out.println();
+
+        Pato patoGoma = new PatoDeGoma();
+        patoGoma.mostrar();
+        patoGoma.realizarCuaqueo();
+        patoGoma.realizarVuelo();
+        patoGoma.nadar();
+
+        System.out.println();
+        Pato patoSenuelo = new PatoSenuelo();
+        patoSenuelo.mostrar();
+        patoSenuelo.realizarCuaqueo();
+        patoSenuelo.realizarVuelo();
+        patoSenuelo.nadar();
+
+        System.out.println();
+        Pato patoCabezaRoja = new PatoCabezaRoja();
+        patoCabezaRoja.mostrar();
+        patoCabezaRoja.realizarCuaqueo();
+        patoCabezaRoja.realizarVuelo();
+        patoCabezaRoja.nadar();
+
     }
     
 }
