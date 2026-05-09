2c21ac392b1bf3439db18c0704c707a4a2ebdef7
Kevin Rujel <Kevinrujel26@gmail.com>
2026-05-09 17:43:08 -0500

    Se creo la clase PatoReal y se creo sus metodos

---
+++ src/Adapter/PatoReal.java
@@ -0,0 +1,14 @@
+package Adapter;
+
+public class PatoReal {
+
+    public void cuaquear() {
+        System.out.println("Cuac Cuac");
+
+    }
+
+    public void volar() {
+        System.out.println("Estoy volando");
+
+    }
+}
