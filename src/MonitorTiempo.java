64742d5d4ddb8253ede83cf34799c1a3dea137c1
Kevin Rujel <Kevinrujel26@gmail.com>
2026-05-16 23:08:16 -0500

    se implemento la clase MonitorTiempo agregando list<Observer> y con los metodos agregar, eliminar, notificar y setTemperatura

--- src/MonitorTiempo.java
+++ src/MonitorTiempo.java
@@ -1,19 +1,33 @@
-public class MonitorTiempo {
-    float temperatura;
-    Pantalla pantalla;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MonitorTiempo(Pantalla pantalla, float temperatura){
-        this.pantalla= pantalla;
-        this.temperatura=temperatura;
+public class MonitorTiempo implements Subject {
 
+    private List<Observer> observers = new ArrayList<>();
+    private float temperatura;
+
+    @Override
+    public void agregarObserver(Observer o) {
+        observers.add(o);
     }
 
-    float getTemperatura(){
-        return temperatura;
+    @Override
+    public void eliminarObserver(Observer o) {
+        observers.remove(o);
     }
 
-    public void mostrarEnPantalla(){
-        pantalla.mostrarEnPantalla(temperatura);
+    @Override
+    public void notificarObservers() {
 
+        for (Observer o : observers) {
+            o.actualizar(temperatura);
+        }
+    }
+
+    public void setTemperatura(float temperatura) {
+
+        this.temperatura = temperatura;
+
+        notificarObservers();
     }
 }
